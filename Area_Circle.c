b08042ca4bcb1e0709aa9169c7e1673d1cb88207
Sabbir Ahmed Shameem <145862004+SAShameem@users.noreply.github.com>
2024-04-05 22:34:31 +0600

    Add files via upload

---
+++ Area_Circle.c
@@ -0,0 +1,14 @@
+//Area of circle:
+
+#include<stdio.h>
+int main (){
+    float radious,area;
+    printf("Enter Radious:");
+    scanf("%f",&radious);
+
+    area=3.1416*radious*radious;
+
+    printf("Area: %.2f",area);
+
+return 0;
+}
