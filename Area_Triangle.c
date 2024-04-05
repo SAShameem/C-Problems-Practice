40dd9c58b725d7f6f6a1c3a75060f64b4e3252da
Sabbir Ahmed Shameem <145862004+SAShameem@users.noreply.github.com>
2024-04-05 22:35:12 +0600

    Add files via upload

---
+++ Area_Triangle.c
@@ -0,0 +1,16 @@
+// Area of triangle
+#include<stdio.h>
+int main(){
+    float base,height,area;
+    printf("Enter Base:");
+    scanf("%f",&base);
+    printf("Enter Height: ");
+    scanf("%f",&height);
+
+    area=0.5*base*height;
+
+    printf("Area= %.2f",area);
+
+
+return 0;
+}
