092be669deefc07885cf2c0fbbc3d18ec2467585
Sabbir Ahmed Shameem <145862004+SAShameem@users.noreply.github.com>
2024-04-06 23:51:04 +0600

    Add files via upload

---
+++ ASCII2char.c
@@ -0,0 +1,12 @@
+// ASCII value to character
+#include<stdio.h>
+int main(){
+    int ascii;
+    printf("Enter a ASCII value:");
+    scanf("%d",&ascii);
+
+    printf("Character of ASCII value %d is %c \n",ascii,ascii);
+
+
+return 0;
+}
