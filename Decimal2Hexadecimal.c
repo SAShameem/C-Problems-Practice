b9620464a23a01048d8d9813ac63fc9ed4288fc4
Sabbir Ahmed Shameem <145862004+SAShameem@users.noreply.github.com>
2024-04-07 16:11:03 +0600

    Add files via upload

---
+++ Decimal2Hexadecimal.c
@@ -0,0 +1,12 @@
+// Number Conversion : Decimal to Hexa-Decimal
+#include <stdio.h>
+int main(){
+    int dcml;
+    printf("Enter a Decimal Number :");
+    scanf("%d",&dcml);
+
+    printf("The Hexa-Decimal Num will be: %x",dcml);
+
+
+return 0;
+}
