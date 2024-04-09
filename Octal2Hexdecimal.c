e1085121219ccab97fd2fed05afc32664be4a6b7
Sabbir Ahmed Shameem <145862004+SAShameem@users.noreply.github.com>
2024-04-09 23:25:13 +0600

    Add files via upload

---
+++ Octal2Hexdecimal.c
@@ -0,0 +1,13 @@
+// Number Conversion : Octal to Hexa-Decimal
+#include <stdio.h>
+int main(){
+    int oct;
+    printf("Enter a Octal Number :");
+    scanf("%o",&oct);
+
+    printf("The Hexa-Decimal Num will be: %x",oct);
+
+
+return 0;
+}
+
