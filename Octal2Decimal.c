8fe84ab6f494eb8edafe6cbe672a6b5e624538be
Sabbir Ahmed Shameem <145862004+SAShameem@users.noreply.github.com>
2024-04-09 23:24:54 +0600

    Add files via upload

---
+++ Octal2Decimal.c
@@ -0,0 +1,12 @@
+// Number Conversion : Octal to Decimal
+#include <stdio.h>
+int main(){
+    int octal;
+    printf("Enter a Octal Number :");
+    scanf("%o",&octal);
+
+    printf("The Decimal Num will be: %d",octal);
+
+
+return 0;
+}
